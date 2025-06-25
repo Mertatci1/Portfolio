a52d814bc982768181ab56fd676273d8da1e015c
Mertatci1 <111279307+Mertatci1@users.noreply.github.com>
2025-06-25 22:20:57 +0300

    Ahmet Mert Atci portfolio website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // React JS ve JSX dosyaları burada taranacak
+    './index.html',        
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
